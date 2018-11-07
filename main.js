77788b7bb4d487ad140f164759360197b51820fe
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-07 19:29:48 +0800

    Added base files

---
+++ main.js
@@ -0,0 +1,25 @@
+var api_key = '04a86ab529b55b53b900403b9530d0e05a753b7502b8eb1a7'
+
+var app = new Vue({
+
+    el: ".app",
+    
+    data(){
+        return{
+            word: "",
+            data: [],
+        }
+    },
+
+    methods: {
+        getMeaning(word){
+            fetch("http://api.wordnik.com/v4/word.json/" + word + "/definitions?api_key=" + api_key)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+                this.data = json
+            })
+        }
+    }
+    
+})
